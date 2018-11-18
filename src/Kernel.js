3190f951e5269b351766a862aace19ad8f30d422
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-11-18 15:44:43 +0100

    created folder structure

---
+++ src/Kernel.js
@@ -0,0 +1,11 @@
+import {Router, Stack, Scene,Drawer} from 'react-native-router-flux';
+
+const Kernel = () => {
+    return (
+        <Router>
+
+        </Router>
+    )
+}
+
+export default Kernel;
