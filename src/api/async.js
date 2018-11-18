e2a6e5230d3def80c3069cac34037f53745c0181
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-11-18 15:47:44 +0100

    added asyn.js

---
+++ src/api/async.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {AsyncStorage} from 'react-native';
+
+const key = "OOnka__opeit986ihhi";
+
+export const loadDb = () => {
+    return AsyncStorage.setItem(key, JSON.stringify(true));
+}
+
+export const isDbLoaded = async () => {
+    try {
+        const loaded = await AsyncStorage.getItem(key);
+        if(JSON.parse(loaded)) return true;
+        return false;
+    }catch(error){
+        console.log('error checking if DB is Loaded')
+    }
+}
